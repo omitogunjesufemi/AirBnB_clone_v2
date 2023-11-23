75ccebc5a081bb4cb520dda5dffe02c4b025b160
omitogunjesufemi <omitogunjesufemi@gmail.com>
2023-11-23 02:18:33 +0100

    Fixed pycodestyle errors

--- models/base_model.py
+++ models/base_model.py
@@ -30,14 +30,18 @@ class BaseModel:
                 kwargs['id'] = str(uuid.uuid4())
 
             if 'created_at' in kwargs:
-                kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                         '%Y-%m-%dT%H:%M:%S.%f')
+                kwargs['created_at'] = datetime.strptime(
+                    kwargs['created_at'],
+                    '%Y-%m-%dT%H:%M:%S.%f'
+                )
             else:
                 self.created_at = datetime.now()
 
             if 'updated_at' in kwargs:
-                kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                         '%Y-%m-%dT%H:%M:%S.%f')
+                kwargs['updated_at'] = datetime.strptime(
+                    kwargs['updated_at'],
+                    '%Y-%m-%dT%H:%M:%S.%f'
+                )
             else:
                 self.updated_at = datetime.now()
 
