ba58546db52423b7ad708cf96d93615909067a65
Jesufemi Omitogun <omitogunjesufemi@gmail.com>
2023-12-27 13:26:52 +0100

    displays C followed by the value of the text variable

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+A script that starts a Flask web application
+Listening on 0.0.0.0, port 5000
+
+Routes:
+   /: displays Hello HBNB!
+   /hbnb: displays HBNB
+   /c/<text>: display C followed by the value of the text variable
+   (replace underscore _ symbols with a space
+"""
+from flask import Flask
+
+hbnb = Flask(__name__)
+
+
+@hbnb.route("/", strict_slashes=False)
+def hello_hbnb():
+    """ Displays Hello HBNB! """
+    return ("Hello HBNB!")
+
+
+@hbnb.route("/hbnb", strict_slashes=False)
+def hbnb_print():
+    """ Displays HBNB """
+    return ("HBNB")
+
+
+@hbnb.route("/c/<text>", strict_slashes=False)
+def display_c(text):
+    """Displays C follow by value of text """
+    return (f"C {text}")
+
+
+if __name__ == "__main__":
+    hbnb.run(host="0.0.0.0", port=5000)
