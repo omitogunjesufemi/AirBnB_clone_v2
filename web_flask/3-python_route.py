67241d25a159f7f080c8624b69a299a6cc53f03c
Jesufemi Omitogun <omitogunjesufemi@gmail.com>
2023-12-27 13:40:00 +0100

    Adding default values in route

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""
+A script that starts a Flask web application
+Listening on 0.0.0.0, port 5000
+
+Routes:
+   /: displays Hello HBNB!
+   /hbnb: displays HBNB
+   /c/<text>: display C followed by the value of the text variable
+   (replace underscore _ symbols with a space)
+   /python/<text>: display Python followed by the value of the text
+   variable (replace underscore with a space) [text default: is cool]
+
+"""
+from flask import Flask
+
+hbnb = Flask(__name__)
+
+
+@hbnb.route("/", strict_slashes=False)
+def hello_hbnb():
+    """ Displays Hello HBNB! """
+    return ("Hello HBNB!")
+
+
+@hbnb.route("/hbnb", strict_slashes=False)
+def hbnb_print():
+    """ Displays HBNB """
+    return ("HBNB")
+
+
+@hbnb.route("/c/<text>", strict_slashes=False)
+def display_c(text):
+    """Displays C follow by value of text """
+    text = text.replace("_", " ")
+    return (f"C {text}")
+
+
+@hbnb.route("/python/", defaults={"text": "is cool"},
+            strict_slashes=False)
+@hbnb.route("/python/<text>", strict_slashes=False)
+def display_python(text):
+    """Displays Python follow by value of text """
+    text = text.replace("_", " ")
+    return (f"Python {text}")
+
+
+if __name__ == "__main__":
+    hbnb.run(host="0.0.0.0", port=5000)
