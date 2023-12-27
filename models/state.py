7b4208ecfe4ef7ba257bf829a10471db5f2453ad
Jesufemi Omitogun <omitogunjesufemi@gmail.com>
2023-12-27 23:36:29 +0100

    Updating some part of our engine

--- models/state.py
+++ models/state.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+import os
 from models.base_model import BaseModel, Base
 from models.city import City
 from sqlalchemy import Column, String
@@ -13,12 +14,14 @@ class State(BaseModel, Base):
     cities = relationship("City", backref="state",
                           cascade="all, delete, delete-orphan")
 
-    @property
-    def cities(self):
-        """Getter for cities attribute when in fileStorage """
-        cities_dict = {}
-        all_cities = storage.all(City)
-        for key, value in all_cities.items():
-            if (value.state_id == self.id):
-                cities_dict[key] = value
-        return cities_dict
+    if os.getenv("HBNB_TYPE_STORAGE") != "db":
+        @property
+        def cities(self):
+            """Getter for cities attribute when in fileStorage """
+            cities_list = []
+            from models import storage
+            all_cities = storage.all(City)
+            for key, value in all_cities.items():
+                if (value.state_id == self.id):
+                    cities_list.append(value)
+            return cities_list
