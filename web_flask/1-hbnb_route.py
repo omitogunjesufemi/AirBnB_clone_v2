2501e88bd42336b16c834e90838b5e414c5e733d
Jesufemi Omitogun <omitogunjesufemi@gmail.com>
2023-12-27 12:45:59 +0100

    Added strict slashes options and wrote the right implementation of the routes

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -13,5 +13,11 @@ def hello_hbnb():
     return ("Hello HBNB!")
 
 
+@hbnb.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Route to display Hello HBNB!"""
+    return ("HBNB!")
+
+
 if __name__ == "__main__":
     hbnb.run(host="0.0.0.0", port=5000)
