981dc503ed8f8fd889efd7703bad47b8bda3f28d
Jesufemi Omitogun <omitogunjesufemi@gmail.com>
2023-12-27 12:31:43 +0100

    Added strict_slashes options in route

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 """
 A script that starts a Flask web application
+Routes:
+   /: displays Hello HBNB
 """
 from web_flask import hbnb
 
 
-@hbnb.route("/")
+@hbnb.route("/", strict_slashes=False)
 def hello_hbnb():
     return ("Hello HBNB!")
 
--- web_flask/__init__.py
+++ web_flask/__init__.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-"""This initializes the app
+"""
+This initializes the app
+The app listens on 0.0.0.0, port 5000
+
 """
 from flask import Flask
 
 
 hbnb = Flask(__name__)
-hbnb.url_map.strict_slashes = False
